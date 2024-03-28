ba5ab9e27199615810fb0711021222ce0766741a
sreesanker <sreeshankerthekkan@gmail.com>
2024-03-28 17:44:19 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
- const app =express()
+const app = express()
 import mongoose from 'mongoose'
 import env from 'dotenv'
 const PORT = 9000
+const MONGO =
+  'mongodb+srv://registration:register@cluster0.udprahb.mongodb.net/'
 env.config()
 import router from './Router/studentRouter.js'
 import error from './Middleware/middleware.js'
@@ -10,18 +12,15 @@ import error from './Middleware/middleware.js'
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/user',router)
+app.use('/user', router)
 app.subscribe(error)
 
+const dbConnection = () => {
+  mongoose.connect(MONGO)
+  console.log('conneted to mongodb')
+}
 
-const dbConnection =()=>{ 
-
-    mongoose.connect(process.env.MONGO)
-    console.log("conneted to mongodb");
-
-} 
-
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    dbConnection()
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+  dbConnection()
 })
